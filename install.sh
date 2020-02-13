c90a78512c0fe7663b8e93ba0277476ee8fa82ec
Paul Houser <houserp@wwu.edu>
2020-02-13 12:44:33 -0800

    install script

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+FILEPATH=$(readlink -f "reTheme")
+
+ln -s $FILEPATH /usr/local/bin/reTheme
+
+# make the directory for the lightdm background image with the correct permissions.
+if ! [ -d /usr/share/wallpapers ]; then
+  mkdir /usr/share/wallpapers
+fi
+chmod 777 /usr/share/wallpapers
+
+# generate the config for slick-greeter if it doesn't already exist
+slickconf="/etc/lightdm/slick-greeter.conf"
+if ! [[ -f $slickconf ]]; then
+  touch $slickconf
+  echo "[Greeter]" >> $slickconf
+  echo "background=/usr/share/wallpapers/wal" >> $slickconf
+else
+  sed -i.slickbk "s/background=.*/background=\/usr\/share\/wallpapers\/wal/g" /etc/lightdm/slick-greeter.conf
+fi
